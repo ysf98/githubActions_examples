1db767422363f6a7585f08d9038f663a0b32fcde
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-01-16 21:28:03 +0100

    telegram

---
+++ index.js
@@ -0,0 +1,32 @@
+// Importamos la librería node-telegram-bot-api 
+const TelegramBot = require('node-telegram-bot-api');
+
+// Creamos una constante que guarda el Token de nuestro Bot de Telegram que previamente hemos creado desde el bot @BotFather
+const token = '5320190038:AAGkhj2IuLbHoK7Pc4o5nSBcInAWp-uaclw';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Workflow ejecutado correctamente tras el último commit. Saludos ' + msg.from.frist_name);
+});
+  bot.sendMessage(chatId, 'Mensaje enviado');
+});
