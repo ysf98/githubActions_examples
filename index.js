e5c63c2762aa2ed338f3f0b359c1669a3cd18a5f
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-01-26 13:40:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 // replace the value below with the Telegram token you receive from @BotFather
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
