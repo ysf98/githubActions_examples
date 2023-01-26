e34bdd219839aef855e75feeb44bc78ae763bf7c
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-01-26 18:47:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 require ('dotenv').config()
 
-//Importamos libreria telegram
-const telegram = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 
-//Creamos el bot
-const bot = new telegram(process.env.TELEGRAM_TOKEN)
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
-const texto = async () => {
-  return "Workflow ejecutado correctamente tras el último commit. Saludos "+ process.env.NOMBRE;
-}
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-const main = async () => {
-  const mensaje = await texto();
-  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, mensaje);
-  console.log(mensaje);
-  
-    // Matches "/echo [whatever]"
-  bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-    const resp = match[1]; // the captured "whatever"
+  const resp = match[1]; // the captured "whatever"
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, resp);
-  });
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, resp);
+});
 
-  // Listen for any kind of message. There are different kinds of
-  // messages.
-  bot.on('message', (msg) => {
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
 
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, 'Received your message');
-  });
-}
-
-
-
-main();
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, 'Received your message');
+});
