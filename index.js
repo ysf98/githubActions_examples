2d94a6b5a5ddf9a1f2d9632dbeb7ed0ebb87bf83
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-01-26 13:54:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require ('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+const bot = new TelegramBot(token, {polling: true});
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -12,7 +12,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
-  const chatId = (process.env.TELEGRAM_CHAT_ID);
+  const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
