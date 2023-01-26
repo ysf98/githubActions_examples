7162a533aad2951702955e0cfae79bc347c43146
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-01-26 18:42:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,25 +14,27 @@ const main = async () => {
   const mensaje = await texto();
   bot.sendMessage(process.env.TELEGRAM_CHAT_ID, mensaje);
   console.log(mensaje);
+  
+    // Matches "/echo [whatever]"
+  bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, resp);
+  });
+
+  // Listen for any kind of message. There are different kinds of
+  // messages.
+  bot.on('message', (msg) => {
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, 'Received your message');
+  });
 }
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, 'Received your message');
-});
 
 main();
