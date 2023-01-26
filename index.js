329cef4b65e633e60731989f27357fa11481c8c8
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-01-26 11:37:42 +0000

    commit

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-// Importamos la librería node-telegram-bot-api 
-const TelegramBot = require('node-telegram-bot-api');
+require ('dotenv').config()
 
-// Creamos una constante que guarda el Token de nuestro Bot de Telegram que previamente hemos creado desde el bot @BotFather
-const token = '5961336306:AAEI4OGFIEKRzXYNH8dUU2UsCKvm1GdFTTc';
+//Importamos libreria telegram
+const telegram = require('node-telegram-bot-api')
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+//Creamos el bot
+const bot = new telegram(process.env.TELEGRAM_TOKEN)
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+const texto = async () => {
+  return "Workflow ejecutado correctamente tras el último commit. Saludos ";
+}
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+const main = async () => {
+  const mensaje = await texto();
+  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, mensaje);
+  console.log(mensaje);
+}
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Workflow ejecutado correctamente tras el último commit. Saludos ' + msg.from.frist_name);
-  bot.sendMessage(chatId, 'Mensaje enviado');
-});
+main();
